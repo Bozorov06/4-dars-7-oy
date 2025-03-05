3b2d7801d76eb1dc1a760e84ea6301d62498ba29
Bozorov06 <qoyilbekbozorov160@gmail.com>
2025-03-05 10:17:59 +0500

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const Navbar = () => {
-// 	return (
-// 		<div className='container border-b-2 border-primary flex justify-between items-center py-3'>
-// 			<a href='/' className='flex items-center gap-5 text-primary'>
-// 				<img src='/logo.svg' alt='logo' />
-// 				<div>
-// 					<span className='block text-3xl font-semibold'>Red Clothes</span>
-// 					<span className='text-sm'>Магазин одежды для практики </span>
-// 				</div>
-// 			</a>
-// 			<div className='flex gap-5 items-center text-primary text-2xl'>
-// 				<a href='/'>
-// 					<i className='fa me-2 fa-shopping-cart'></i>
-// 					<span>30 595 ₽</span>
-// 				</a>
-// 				<Link to={'/wishlist'}>
-// 					<i className='fa-regular fa-heart'></i>
-// 				</Link>
-// 				<a href='/'>
-// 					<i className='fa-regular fa-user'></i>
-// 				</a>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default Navbar;
-
-
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
